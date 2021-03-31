f18ac20511da97993a1002760ad6267eb39f6b2e
chenwenxing <chenwenxing@inke.cn>
2021-03-31 22:50:33 +0800

    最大子序和

---
+++ 53-maxSubArray.php
@@ -0,0 +1,18 @@
+<?php
+
+function maxSubArray($nums) {
+    $max = $nums[0];
+    $cou = count($nums);
+
+    for ($i = 0; $i < $cou; $i++) {
+        $sum = $nums[$i];
+        for ($j = $i+1; $j < $cou; $j++) {
+            $sum += $nums[$j];
+            if ($sum > $max) $max = $sum;
+        }
+    }
+    return $max;
+}
+
+$nums = [-2,1,-3,4,-1,2,1,-5,4];
+var_dump(maxSubArray($nums));
