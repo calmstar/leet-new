c9e0fdf25189e4ac1c3d8580a6e9d8e9c3d596e4
chenwenxing <chenwenxing@inke.cn>
2021-04-01 22:13:09 +0800

    最大子序和-暴力法

--- 53-maxSubArray.php
+++ 53-maxSubArray.php
@@ -1,15 +1,18 @@
 <?php
 
+// 暴力法
 function maxSubArray($nums) {
     $max = $nums[0];
     $cou = count($nums);
 
     for ($i = 0; $i < $cou; $i++) {
         $sum = $nums[$i];
+        if ($sum > $max) $max = $sum;
         for ($j = $i+1; $j < $cou; $j++) {
             $sum += $nums[$j];
             if ($sum > $max) $max = $sum;
         }
+        if ($sum > $max) $max = $sum;
     }
     return $max;
 }
