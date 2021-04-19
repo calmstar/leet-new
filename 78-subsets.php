6637caafbd2febdfbb83faaae8dae9ea2c6dc947
chenwenxing <chenwenxing@inke.cn>
2021-04-19 22:44:35 +0800

    子集

---
+++ 78-subsets.php
@@ -0,0 +1,18 @@
+<?php
+function subsets($nums) {
+    $numsLen=sizeof($nums);
+    if($numsLen==0)//递归终止
+        return [[]];
+
+    $ret=[];
+    foreach($nums as $k=>$v){
+        unset($nums[$k]);
+        $res=$this->subsets($nums);//递归
+        foreach($res as $vv){//处理返回结果
+            array_unshift($vv,$v);
+            $ret[]=$vv;
+        }
+    }
+    $ret[]=[]; //末尾加个[]
+    return $ret;//返回结果
+}
