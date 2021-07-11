8df95c41d8ab01463022ae5953d9762f14f89730
chenwenxing <chenwenxing@inke.cn>
2021-07-11 14:51:32 +0800

    移动零

--- 283-moveZeroes.php
+++ 283-moveZeroes.php
@@ -21,3 +21,24 @@ class Solution {
         }
     }
 }
+
+/**
+// 指针指向法
+class Solution {
+function moveZeroes(&$nums) {
+    $cou = count($nums);
+    // i指针每次都移动。。j指针在数字为0的地方停下来，用于跟i指针指向的非0数字进行替换
+    for ($i = 0,$j = 0; $i < $cou; $i++) {
+        if ($nums[$i] !== 0) {
+            $nums[$j] = $nums[$i];
+            $j++;
+        }
+    }
+    // 把从$j到后面的数字全部置为0
+    for ($i = $j; $i < $cou; $i++) {
+        $nums[$i] = 0;
+    }
+
+}
+}
+ */
