30d9a67154469a3c2007557b318eb81b97de95ff
chenwenxing <chenwenxing@inke.cn>
2021-07-22 16:09:02 +0800

    最小覆盖子串-超时暴力版本

---
+++ 76-minWindow.php
@@ -0,0 +1,75 @@
+<?php
+
+class Solution {
+
+
+
+    /**
+     * v1版本会超时
+     * @param String $s
+     * @param String $t
+     * @return String
+     */
+    function minWindowV1($s, $t) {
+        $len = strlen($s);
+        $tlen = strlen($t);
+        if ($len < $tlen) return '';
+
+        $min = $len + 1;
+        $minStr = '';
+        // 循环得到所有子串
+        for ($i = 0; $i < $len; $i++) {
+            for ($j = $i; $j < $len; $j++) {
+                $str = substr($s, $i, $j-$i+1);
+                // 判断字符是否包含$t
+                $res = $this->isIncludeV1($str, $t);
+                if ($res && $min > strlen($str)) {
+                    $min = strlen($str);
+                    $minStr = $str;
+                }
+            }
+        }
+        return $minStr;
+    }
+
+     function isIncludeV1 ($big, $small)
+    {
+        $bLen = strlen($big);
+        $len = strlen($small);
+        if ($len > $bLen) {
+            return false;
+        }
+        $bigArr = [];
+        for ($j = 0; $j < $bLen; $j++) {
+            $bigArr[] = $big[$j];
+        }
+
+        // 判断是否包含
+        for ($i = 0; $i < $len; $i++) {
+            $isHad = false;
+            foreach ($bigArr as $k => $v) {
+                if ($small[$i] == $v) {
+                    $isHad = true;
+                    unset($bigArr[$k]);
+                    break;
+                }
+            }
+            if (!$isHad) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+$s = "ADOBECODEBANC";
+$t = "ABC";
+// 预期结果
+//"BANC"
+
+$res = (new Solution())->minWindow($s, $t);
+//$res = (new Solution())->isInclude("BANC", "ABC");
+var_dump($res);
+
+
+
