d607c2a3c0de034a9220bccc3be5d7140efad881
chenwenxing <chenwenxing@inke.cn>
2021-07-23 20:38:07 +0800

    今天都为--双指针解法--水果成蓝

--- 904-totalFruit.php
+++ 904-totalFruit.php
@@ -6,10 +6,36 @@ class Solution {
      * @return Integer
      */
     function totalFruit($fruits) {
-        $max = 0; // 篮子目前装的最大水果数量
         $left = 0;
         $right = 0;
-        $map = [];
-    }
+        $window = [];
+        $cou = count($fruits);
+        $max = 0;
+        while ($right < $cou) {
+            // 往右边扩展
+            $rTmp = $fruits[$right];
+            $right++;
+            $window[$rTmp]++; // 存储hash状态
 
+            // 种类大于两种，则开始进行左边收缩，收缩成符合条件
+            while (count($window) > 2) {
+                $lTmp = $fruits[$left];
+                $left++;
+                $window[$lTmp]--;// 变更hash状态
+                if (!$window[$lTmp]) {
+                    unset($window[$lTmp]);
+                }
+            }
+            // 计算总和
+            $total = 0;
+            foreach ($window as $v) {
+                $total += $v;
+            }
+            $max = max($max, $total);
+        }
+        return $max;
+    }
 }
+$a = [0,1,2,2];
+$res = (new Solution())->totalFruit($a);
+var_dump($res);
