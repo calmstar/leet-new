0dfba0635ab0368213044e76b655fe4d9244e1a6
chenwenxing <chenwenxing@inke.cn>
2021-07-23 10:53:13 +0800

    水果成蓝

--- 904-totalFruit.php
+++ 904-totalFruit.php
@@ -6,6 +6,10 @@ class Solution {
      * @return Integer
      */
     function totalFruit($fruits) {
-
+        $max = 0; // 篮子目前装的最大水果数量
+        $left = 0;
+        $right = 0;
+        $map = [];
     }
+
 }
