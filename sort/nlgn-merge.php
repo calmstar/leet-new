a98bdacda881b2682f72708986f7c9cb54594bea
chenwenxing <chenwenxing@inke.cn>
2021-07-26 17:53:47 +0800

    归并排序

---
+++ sort/nlgn-merge.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * https://www.cnblogs.com/chengxiao/p/6194356.html
+ *
+ * 归并排序，从小到大进行排序
+ *
+ * 时间复杂度相关，看总结图
+ *
+ * @param $nums
+ * @return array
+ */
+function mergeSort ($nums)
+{
+    $cou = count($nums);
+    if ($cou < 2) return $nums;
+
+    $mid = intval($cou / 2);
+    $left = array_slice($nums, 0, $mid);
+    $right = array_slice($nums, $mid, $cou - $mid);
+
+    $left = mergeSort($left); // 左边拆分完后开始分治，直到出口
+    $right = mergeSort($right); // 右边拆分完毕开始分治，直到出口
+
+    // 递归到底部，此处开始合并。
+    // "分"结束，开始"治"
+    return merge($left, $right);
+
+}
+
+function merge ($left, $right)
+{
+    $temp = [];
+    // 将两个分别排序好的数组，进行归并
+    while (count($left) && count($right)) {
+        $temp[] = $left[0] < $right[0] ? array_shift($left) : array_shift($right);
+    }
+    // $left 或者 $right数组可能有剩余的数字
+    return array_merge($temp, $left, $right);
+}
+
+$a = [1,3,2,9,5,4];
+$res = mergeSort($a);
+var_dump($res);
