f2e52e1a5f921141c1c4382280a75f58cddd597c
chenwenxing <chenwenxing@inke.cn>
2021-07-28 22:29:56 +0800

    判断是否回文链表 -- 利用特殊技巧，时间复杂度O(n)  空间复杂度O(1)

--- 234-isPalindrome.php
+++ 234-isPalindrome.php
@@ -12,8 +12,62 @@
  */
 class Solution {
 
+    // ---------- 方法3 ----------
+    // 利用特殊技巧，时间复杂度O(n)  空间复杂度O(1)
+    // https://mp.weixin.qq.com/s/tCgEoOlZKS_ohuTx1VxJ-Q
+    function isPalindrome ($head)
+    {
+       // 1 利用快慢指针找到链表中间节点
+        $fast = $head;
+        $slow = $head;
+        while ($fast !== null && $fast->next !== null) {
+            $slow = $slow->next;
+            $fast = $fast->next->next;
+        }
+        // 如果 $fast 不为空，说明该链表是奇数，slow指针需要往前移动一格才是需要翻转的起点
+        if ($fast !== null) {
+            $slow = $slow->next;
+        }
+        // 开始翻转 slow 之后的节点，返回的$reverseHead在尾部节点
+        $reverseHead =  $this->reverseList($slow);
+        // 翻转之后，$head节点在前面开始遍历，$reverseHead在后面往前遍历，依次比较
+        while ($reverseHead !== null) {
+           if ($reverseHead->val !== $head->val) return false;
+            $head = $head->next;
+            $reverseHead = $reverseHead->next;
+        }
+        return true;
+    }
+
+
+
+
+    // ---------- 方法2 ----------
+
+    private $left = null;
+    // 通过后序遍历：时间和空间复杂度都是O(n)
+    function isPalindromeBackend ($head)
+    {
+        $this->left = $head;
+        return $this->traverse($head);
+    }
+
+    function traverse ($head)
+    {
+        if ($head === null) return true;
+        $res = $this->traverse($head->next);
+        // 后序遍历 ，此时第一次走到这里的时候 $head 是尾部节点，然后开始"归"
+        $res = $res && $head->val == $this->left->val;
+        $this->left = $this->left->next;
+        return $res;
+    }
+
+    // ---------- 方法1 ----------
+
     /**
      * 判断是否回文链表 -- 通过翻转链表值赋予到新链表，注意使用临时指针指向
+     *  --- 也可以将元素值存在栈中，然后出栈依次跟链表比较
+     * 时间和空间复杂度都是O(n)
      * @param ListNode $head
      * @return Boolean
      */
