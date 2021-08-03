9538f2d1cb1c7c9fd533888583f92bef3776a311
chenwenxing <chenwenxing@inke.cn>
2021-08-03 15:24:58 +0800

    树的最小深度-前序和后序遍历解法

--- 111-minDepth.php
+++ 111-minDepth.php
@@ -16,25 +16,42 @@
 class Solution {
 
     /**
-     * 递归解法
+     * 递归解法 -- 后序遍历 -- 计算高度值，在"归"过程计算判断，保存所需值
      * @param TreeNode $root
      * @return Integer
      */
     function minDepth($root) {
-        if ($root == null) {
-            return 0;
-        }
-        $leftMin = $this->minDepth($root->left) + 1;
-        $rightMin = $this->minDepth($root->right) + 1;
-        if ( min($leftMin, $rightMin) == 1 ) {
-            if ($leftMin > $rightMin) {
-                return $leftMin;
-            } else {
-                return $rightMin;
-            }
+        if ($root === null) return 0;
+        $left = $this->minDepth($root->left);
+        $right = $this->minDepth($root->right);
+        // 如果有一个最小值为0，说明树是倾斜一方的，应该取最大值的那个（因为最小的深度指的叶子节点，是都没有左右儿子节点的）
+        if (min($left, $right) == 0) {
+            return max($left, $right) + 1;
         } else {
-            return min($leftMin, $rightMin);
+            return min($left, $right) + 1;
         }
+    }
+
+    // 深度遍历 -- 前序遍历 -- 计算深度，在"递"的过程进行计算判断保存值，"归"过程放
+    private $res = null;
+    function minDepthV2 ($root)
+    {
+        if ($root === null) return 0;
+        $this->getMin($root, 1);
+        return $this->res;
+    }
+    function getMin ($root, $deep)
+    {
+        if ($root === null) return;
+        if ($root->left === null && $root->right === null) { // 当左右子树都为空的时候，才是到了叶子节点
+            if ($this->res !== null) {
+                $this->res = min($this->res, $deep); // 取到达叶子节点最小的深度值
+            } else {
+                $this->res = $deep; // 初始化状态，不需要比较
+            }
+        }
+        $this->getMin($root->left, $deep+1);
+        $this->getMin($root->right, $deep+1);
     }
 
     // 通过栈进行解决，-- 深度优先遍历 -- 搞不了
