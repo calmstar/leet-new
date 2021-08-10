4be01aa9a11e7a5500572604796d7fcd5a55c49c
chenwenxing <chenwenxing@inke.cn>
2021-08-10 19:55:29 +0800

    最近的公共祖先

--- 236-lowestCommonAncestor.php
+++ 236-lowestCommonAncestor.php
@@ -11,12 +11,24 @@
 
 class Solution {
     /**
+     * https://mp.weixin.qq.com/s/9RKzBcr3I592spAsuMH45g
      * @param TreeNode $root
      * @param TreeNode $p
      * @param TreeNode $q
      * @return TreeNode
      */
     function lowestCommonAncestor($root, $p, $q) {
+        if ($root === null) return null;
+        if ($root === $p || $root===$q) return $root;
 
+        $left = $this->lowestCommonAncestor($root->left, $p, $q);
+        $right = $this->lowestCommonAncestor($root->right, $p, $q);
+        if ($left === null && $right === null) {
+            return null;
+        }
+        if ($left !== null && $right !== null) { // // 如果p和q都在以root为根的树中，那么left和right一定分别是p和q（从 base case 看出来的）。
+            return $root;
+        }
+        return $left === null ? $right : $left;
     }
 }
