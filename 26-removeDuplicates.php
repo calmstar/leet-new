b05f83ff18ce17b44f0a9a2c7f9229021a0927ff
chenwenxing <chenwenxing@inke.cn>
2021-09-06 11:42:02 +0800

    快慢指针移除重复的元素

--- 26-removeDuplicates.php
+++ 26-removeDuplicates.php
@@ -1,5 +1,6 @@
 <?php
 
+// php特有的方法，不涉及数组的元素搬移
 function removeDuplicates(&$nums) {
     $val = '';
     foreach ($nums as $k => $v) {
@@ -12,4 +13,22 @@ function removeDuplicates(&$nums) {
         $val = $v;
     }
     return count($nums);
+}
+
+// 快慢指针法 -- 思想，所有语言共有
+// 维护一个快慢指针，[0, slow]区间的为不重复元素集合
+function removeDuplicatesV2(&$nums)
+{
+    if (empty($nums)) return [];
+    $slow = 0;
+    $fast = 0;
+    $cou = count($nums);
+    while ($fast < $cou) {
+        if ($nums[$fast] != $nums[$slow]) { // nums是排序好的
+            $slow++;
+            $nums[$slow] = $nums[$fast];
+        }
+        $fast++;
+    }
+    return $slow+1;
 }
