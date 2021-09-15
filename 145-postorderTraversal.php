64b8876474884532a988e0e232682a588df2410b
chenwenxing <chenwenxing@inke.cn>
2021-09-15 12:00:56 +0800

    后序遍历-迭代法

--- 145-postorderTraversal.php
+++ 145-postorderTraversal.php
@@ -34,5 +34,33 @@ class Solution {
         $this->res[] = $root->val;
     }
 
+    // --------- 迭代法 ---------
+
+    function postorderTraversalV2 ($root)
+    {
+        $res = [];
+        if ($root === null) return $res;
+        $stack = [];
+        array_push($stack, $root);
+        while (!empty($stack)) {
+            $tmp = array_pop($stack);
+            $res[] = $tmp->val;
+
+            $tmp->left !== null && array_push($stack, $tmp->left); // 将先序遍历的left right顺序反过来，最终结果res变为 根右左
+            $tmp->right !== null  && array_push($stack, $tmp->right);
+        }
+        // res变为 根右左，将数组反转变为 左右根
+        $right = count($res)-1;
+        $left = 0;
+        while ($left <= $right) {
+            $tmp = $res[$right];
+            $res[$right] = $res[$left];
+            $res[$left] = $tmp;
+            $left++;
+            $right--;
+        }
+        return $res;
+    }
+
 
 }
