dc93154fa3cca68131b04034c01b8a45751bdd85
chenwenxing <chenwenxing@inke.cn>
2021-09-16 11:03:29 +0800

    N叉树的前序遍历--迭代法

--- 589-preorder.php
+++ 589-preorder.php
@@ -15,7 +15,7 @@ class Solution {
 
     private $res = [];
     /**
-     * N 叉树的前序遍历: 根左右
+     * N 叉树的前序遍历: 根左右 (迭代法和递归法都可参考二叉树的前序遍历)
      * https://mp.weixin.qq.com/s/JWmTeC7aKbBfGx4TY6uwuQ
      *
      * 递归法
@@ -36,5 +36,25 @@ class Solution {
         }
     }
 
-    // 迭代法
+    // -------------- 分割线 ---------------
+    // 迭代法 -- 类似二叉树的前序遍历迭代法
+    function preorderV2 ($root)
+    {
+        $res = [];
+        if ($root === null) return $res;
+        $stack = [];
+        array_push($stack, $root);
+        while (!empty($stack)) {
+            $tmp = array_pop($stack);
+            $res[] = $tmp->val;
+
+            $maxIndex = count($tmp->children) - 1;
+            for ($i = $maxIndex; $i >= 0; $i--) {
+                // 类似二叉树遍历，先右后左，栈出来的顺序就会先左后右
+                $tmp->children[$i] && array_push($stack, $tmp->children[$i]);
+            }
+        }
+        return $res;
+    }
+
 }
