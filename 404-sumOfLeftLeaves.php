c56026d4ae4fa4c7c98a2c2cf04ff146f2c6608b
chenwenxing <chenwenxing@inke.cn>
2021-09-21 13:21:23 +0800

    左叶子之和-后序遍历

--- 404-sumOfLeftLeaves.php
+++ 404-sumOfLeftLeaves.php
@@ -3,6 +3,7 @@
 class Solution {
 
     /**
+     * 前序遍历
      * @param TreeNode $root
      * @return Integer
      */
@@ -20,6 +21,24 @@ class Solution {
         }
         $node->left && $this->solve($node->left, true);
         $node->right && $this->solve($node->right, false);
+    }
+
+    // ------------------- 分割线 -------------------
+
+    // 后序遍历，因为要通过左右树的节点总值来返回，从下往上推
+    function sumOfLeftLeavesV2 ($root)
+    {
+        if ($root === null) return 0;
+
+        $leftValue = $this->sumOfLeftLeavesV2($root->left);
+        $rightValue = $this->sumOfLeftLeavesV2($root->right);
+
+        $midValue = 0;
+        if ($root->left && !$root->left->left && !$root->left->right) {
+            $midValue = $root->left->val;
+        }
+        $sum = $midValue + $leftValue + $rightValue;
+        return $sum;
     }
 
 }
