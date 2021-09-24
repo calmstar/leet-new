cbdeb052777064dc3fc7a05834cee8b006bf20ea
chenwenxing <chenwenxing@inke.cn>
2021-09-24 11:58:36 +0800

    合并两棵二叉树

--- 617-mergeTrees.php
+++ 617-mergeTrees.php
@@ -21,7 +21,7 @@ class Solution {
 
     /**
      * 合并两棵二叉树
-     * https://mp.weixin.qq.com/s/tzjZxflogS6UJ3W3QGrIkQ
+     * 自己写的
      * @param TreeNode $root1
      * @param TreeNode $root2
      * @return TreeNode
@@ -35,12 +35,44 @@ class Solution {
         $root1->val += $root2->val;
 
         if ($root1->left || $root2->left) {
+            // 以 $a = [1,2]; $b = [1,null,2]; 为例
+            // 需要用 $root1->left 进行指向，不然 root1 无法指向第三个节点（第三个节点root1是null，需要后面创建，但未产生关联，所以需要返回并指向）
             $root1->left = $this->mergeTrees($root1->left, $root2->left);
         }
         if ($root1->right || $root2->right) {
             $root1->right = $this->mergeTrees($root1->right, $root2->right);
         }
         return $root1;
+    }
+
+    // ------------- 分割线 --------------
+
+    // https://mp.weixin.qq.com/s/tzjZxflogS6UJ3W3QGrIkQ
+    function mergeTreesV2 ($root1, $root2)
+    {
+        if ($root1 === null) return $root2;
+        if ($root2 === null) return $root1;
+
+        $root1->val += $root2->val;  // 前中后序都可以
+        $root1->left = $this->mergeTreesV2($root1->left, $root2->left);
+//         $root1->val += $root2->val;
+        $root1->right = $this->mergeTreesV2($root1->right, $root2->right);
+//         $root1->val += $root2->val;
+        return $root1;
+    }
+
+    // ------------- 分割线 --------------
+
+    // 创建新节点
+    function mergeTreesV3 ($root1, $root2)
+    {
+        if ($root1 === null) return $root2;
+        if ($root2 === null) return $root1;
+        $newNode = new TreeNode(0);
+        $newNode->val = $root1->val + $root2->val; // 前中序遍历都可以
+        $newNode->left = $this->mergeTreesV3($root1->left, $root2->left);
+        $newNode->right = $this->mergeTreesV3($root1->right, $root2->right);
+        return $newNode;
     }
 
 }
