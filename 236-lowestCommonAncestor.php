bc82597e2303ad136e19e99fc9b149dde0f98d4c
chenwenxing <chenwenxing@inke.cn>
2021-09-26 14:30:40 +0800

    二叉树最近的公共祖先

--- 236-lowestCommonAncestor.php
+++ 236-lowestCommonAncestor.php
@@ -12,23 +12,32 @@
 class Solution {
     /**
      * https://mp.weixin.qq.com/s/9RKzBcr3I592spAsuMH45g
+     * https://mp.weixin.qq.com/s/a5cCGw1lY3yTNQoPLr-pGQ
      * @param TreeNode $root
      * @param TreeNode $p
      * @param TreeNode $q
      * @return TreeNode
      */
     function lowestCommonAncestor($root, $p, $q) {
+        // 递归出口，都是针对当前节点的判断，没有使用左右子节点
         if ($root === null) return null;
-        if ($root === $p || $root===$q) return $root;
+        if ($root === $p || $root === $q) return $root;
 
+        // 使用left和right接住，处理后进行return，遍历整棵树
+        // 左操作
         $left = $this->lowestCommonAncestor($root->left, $p, $q);
+        // 右操作
         $right = $this->lowestCommonAncestor($root->right, $p, $q);
+
+        // 单层递归逻辑
+        // 根操作
         if ($left === null && $right === null) {
             return null;
         }
-        if ($left !== null && $right !== null) { // // 如果p和q都在以root为根的树中，那么left和right一定分别是p和q（从 base case 看出来的）。
+        if ($left !== null && $right !== null) {
             return $root;
         }
         return $left === null ? $right : $left;
     }
+
 }
