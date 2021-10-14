8a8289f2f0df64c1f5651647bdfc5e4dc3f29b09
chenwenxing <chenwenxing@inke.cn>
2021-10-14 10:58:21 +0800

    day3- 移除链表元素-虚拟头节点

--- 203-removeElements.php
+++ 203-removeElements.php
@@ -13,10 +13,11 @@ class Solution {
      */
     function removeElements($head, $val)
     {
+        // 删除头节点
         while ($head->val == $val) {
             $head = $head->next;
         }
-        // 中间节点
+        // 删除啊中间节点
         $prev = $head;
         $curr = $head->next;
         while ($curr !== null) {
@@ -30,6 +31,33 @@ class Solution {
             }
         }
         return $head;
+    }
+
+    /**
+     * 增加虚拟头节点,统一逻辑
+     * @param ListNode $head
+     * @param Integer $val
+     * @return ListNode
+     */
+    function removeElementsV2($head, $val)
+    {
+        $dummy = new ListNode(0);
+        $dummy->next = $head;
+        $pre = $dummy;
+        $curr = $dummy->next;
+
+        while ($curr !== null) {
+            if ($curr->val == $val) {
+                // 需要移除
+                $pre->next = $pre->next->next;
+                $curr = $pre->next;
+            } else {
+                $curr = $curr->next;
+                $pre = $pre->next;
+            }
+        }
+
+        return $dummy->next;
     }
 
 }
