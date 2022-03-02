84042fe94cbfc97ab83abc4489ecdccad2054a44
chenwenxing <chenwenxing@inke.cn>
2022-03-02 18:25:32 +0800

    practice-day5-coinChange

--- 322-coinChange.php
+++ 322-coinChange.php
@@ -4,15 +4,15 @@ class Solution {
     /**
      * https://mp.weixin.qq.com/s/Cw39C9MY9Wr2JlcvBQZMcA
      * 动态规划
-     *  最优子结构
-     *  状态转移方程
+     *  最优子结构：10元金额，只需要求得 10-x 金币下的最少硬币数量，加上一个 x 面值的硬币即可。
+     *  状态转移方程：dp[amount] = 1 + dp[amount-coin]
      *  重叠子问题
      *
      * 步骤：
-     *  1 状态
-     *  2 函数定义
-     *  3 选择
-     *  4 baseCase
+     *  1 状态 ：金额的数量变化
+     *  2 函数定义：金额 $amount 下，最少有 coinChange($coins, $amount) 中组装方式
+     *  3 选择：硬币的面值
+     *  4 baseCase： 金额为0时，需要0个硬币； 金额为负数时，无解
      */
 
     /**
@@ -53,9 +53,26 @@ class Solution {
         return $this->memo[$amount];
     }
 
+    // ----------
+    // 迭代法
+    function coinChangeV3($coins, $amount) {
+        $dp = [];
+        $dp[0] = 0;
+        for ($i = 1; $i <= $amount; $i++) {
+            // 其他金币进行初始化
+            $dp[$i] = $amount + 1;
+            foreach ($coins as $coin) {
+                $res = $i - $coin;
+                if ($res < 0) continue;
+                $dp[$i] = min(1 + $dp[$res], $dp[$i]);
+            }
+        }
+        return $dp[$amount] == $amount + 1 ? -1 : $dp[$amount];
+    }
+
 }
 
-$coins = [1, 2, 5];
-$amount = 11;
-$res = (new Solution())->coinChange($coins, $amount);
+$coins = [2];
+$amount = 3;
+$res = (new Solution())->coinChangeV3($coins, $amount);
 var_dump($res);
