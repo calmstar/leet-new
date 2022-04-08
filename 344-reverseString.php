b0bcaeb4dbe358fe7e47d8b2c337ef1c7792eaff
chenwenxing <chenwenxing@inke.cn>
2022-04-08 11:46:22 +0800

    翻转字符串

---
+++ 344-reverseString.php
@@ -0,0 +1,22 @@
+<?php
+class Solution {
+
+    /**
+     * @param String[] $s
+     * @return NULL
+     */
+    function reverseString (&$s) {
+        $len = count($s);
+        if ($len <= 1) return $s;
+        $left = 0;
+        $right = $len - 1;
+        while ($left <= $right) {
+            $temp = $s[$left];
+            $s[$left] = $s[$right];
+            $s[$right] = $temp;
+            $left++;
+            $right--;
+        }
+    }
+
+}
