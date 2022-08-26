b9d799a66d17c75d7ad44b051d2ccc319f34bd42
chenwx <chenwx@flatincbr.com>
2022-08-26 22:59:33 +0800

    全排列

--- go-code/46-permute.go
+++ go-code/46-permute.go
@@ -4,17 +4,21 @@ import (
 	"fmt"
 )
 
-var res [][]int = make([][]int, 0)
+// --------------v1：全局变量注意需要释放,在permute需要重新make，否则append会累加----------------------
+var res [][]int
 
 func permute(nums []int) [][]int {
+	res = make([][]int, 0)
 	var backtrack []int
 	backtracking(backtrack, nums)
 	return res
 }
-
 func backtracking(backtrack []int, nums []int) {
 	if len(backtrack) == len(nums) {
-		res = append(res, backtrack)
+		//res = append(res, backtrack)
+		tmp := make([]int, len(backtrack))
+		copy(tmp, backtrack) // 需要对 backtrack 进行深拷贝到tmp。否则传到res的二维切片中后，如果backtrack有改动的话，会导致res中的值也会发生变化
+		res = append(res, tmp)
 		return
 	}
 	for _, v := range nums {
@@ -22,7 +26,7 @@ func backtracking(backtrack []int, nums []int) {
 			continue
 		}
 
-		backtrack = append(backtrack, v) // 入栈
+		backtrack = append(backtrack, v) // 入栈。 对backtrack进行append会导致其扩容，进而导致切片backtrack存的指针对应的内存地址发生复制，导致res中存的数据发生变化（如果res中的数据不是深拷贝的话）
 		backtracking(backtrack, nums)
 		backtrack = backtrack[0 : len(backtrack)-1]
 	}
@@ -45,7 +49,7 @@ func main() {
 }
 
 // --------------v3----------------------
-// 跟v1比较，就是 res 换成了指针类型传入。 slice虽然本身是引用类型，变量本身存的是值的地址，但是由于append扩容导致地址失效，所以采用指针（即变量值地址的值的地址）
+// res 换成了指针类型传入。 slice虽然本身是引用类型，变量本身存的是值的地址，但是由于append扩容导致地址失效，所以采用指针（即变量值地址的值的地址）
 func permuteV3(nums []int) [][]int {
 	var res [][]int = make([][]int, 0)
 	var backtrack []int
@@ -55,7 +59,9 @@ func permuteV3(nums []int) [][]int {
 
 func backtrackingV3(res *[][]int, backtrack []int, nums []int) {
 	if len(backtrack) == len(nums) {
-		*res = append(*res, backtrack)
+		tmp := make([]int, len(backtrack))
+		copy(tmp, backtrack)
+		*res = append(*res, tmp)
 		return
 	}
 	for _, v := range nums {
@@ -63,7 +69,7 @@ func backtrackingV3(res *[][]int, backtrack []int, nums []int) {
 			continue
 		}
 
-		backtrack = append(backtrack, v) // 入栈
+		backtrack = append(backtrack, v) // 入栈，backtrack进行append也会存在扩容问题，所以上面需要进行深拷贝
 		backtrackingV3(res, backtrack, nums)
 		backtrack = backtrack[0 : len(backtrack)-1]
 	}
