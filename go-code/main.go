7f00d9baf59844c85af4be303cfd00f8df1239da
chenwx <chenwx@flatincbr.com>
2022-09-09 18:55:19 +0800

    test

--- go-code/main.go
+++ go-code/main.go
@@ -4,14 +4,150 @@ import (
 	"bytes"
 	"fmt"
 	"leet-new/go/lodago"
+	"log"
+	"math/rand"
 	"runtime"
 	"strconv"
+	"sync"
+	"testing"
+	"time"
 	"unsafe"
 )
 
-func main() {
-	hhh()
+func init() {
+	// 获取日志文件句柄
+	// 以 只写入文件|没有时创建|文件尾部追加 的形式打开这个文件
+	//logFile, err := os.OpenFile(`./日志文件.log`, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//// 设置存储位置, log. 就会输出到指定的文件位置
+	//log.SetOutput(logFile)
 
+}
+
+func main() {
+	//testConcurrentMap()
+	//testConcurrentSafeMap()
+}
+
+// -------- 性能陷阱 start ---------
+func lastNumsBySlice(origin []int) []int {
+	return origin[len(origin)-2:]
+}
+
+func lastNumsByCopy(origin []int) []int {
+	result := make([]int, 2)
+	copy(result, origin[len(origin)-2:])
+	return result
+}
+
+func generateWithCap(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
+}
+
+func printMem(t *testing.T) {
+	t.Helper()
+	var rtm runtime.MemStats
+	runtime.ReadMemStats(&rtm)
+	t.Logf("%.2f MB", float64(rtm.Alloc)/1024./1024.)
+}
+
+func testLastChars(t *testing.T, f func([]int) []int) {
+	t.Helper()
+	ans := make([][]int, 0)
+	for k := 0; k < 100; k++ {
+		origin := generateWithCap(128 * 1024) // 1M
+		ans = append(ans, f(origin))
+	}
+	printMem(t)
+	_ = ans
+}
+
+func TestLastCharsBySlice(t *testing.T) { testLastChars(t, lastNumsBySlice) }
+func TestLastCharsByCopy(t *testing.T)  { testLastChars(t, lastNumsByCopy) }
+
+// -------- 性能陷阱 end -----------
+
+// -------- 并发写入map问题 -----------
+func testConcurrentMap() {
+	m := make(map[int]int)
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			if err := recover(); err != nil {
+				log.Fatalf("err: %v", err)
+			}
+			m[i] = 10
+		}()
+	}
+	time.Sleep(time.Second)
+	fmt.Println(m)
+}
+
+func testConcurrentSafeMap() {
+	m := &SMap{
+		testMap: make(map[int]int),
+	}
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			if err := recover(); err != nil {
+				log.Fatalf("err: %v", err)
+			}
+			m.writeMap(i, i)
+			log.Println(m.readMap(i))
+		}(i)
+	}
+	time.Sleep(time.Second)
+	//fmt.Println(mMap)
+}
+
+var mMap *SMap
+
+type SMap struct {
+	testMap map[int]int
+	sync.RWMutex
+}
+
+func (l *SMap) readMap(key int) int {
+	l.RLock()
+	res := l.testMap[key]
+	l.RUnlock()
+	return res
+}
+func (l *SMap) writeMap(key int, value int) bool {
+	l.Lock()
+	l.testMap[key] = value
+	l.Unlock()
+	return true
+}
+
+// -------- 并发写入map问题 -----------
+
+func panicCross() {
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Fatalf("hh-test： %v", err)
+			}
+		}()
+		handle()
+	}()
+}
+
+// 跨协程的恐慌处理
+func handle() {
+	divede(1, 0)
+	//go divede(1, 0) // 跨协程
+}
+
+func divede(a int, b int) int {
+	return a / b
 }
 
 func xx() {
